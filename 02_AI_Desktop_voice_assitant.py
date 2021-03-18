2bbf208c358b3580ee945c766d5a205d9af69f46
Ajeet <ajeetkumarabcd10@gmail.com>
2021-03-18 20:12:39 +0530

    speech recognition and using takeCommand fn

--- 02_AI_Desktop_voice_assitant.py
+++ 02_AI_Desktop_voice_assitant.py
@@ -1,10 +1,13 @@
-import pyttsx3 #It is used for conversion of text to speech 
+#Building an application of Desktop Voice Assistant
+
+import pyttsx3 #It is used for conversion of text to speech which is written by programmer in program
 import datetime#Built-in module
+import speech_recognition as sr #This module is used to recognize user voice (mean what they want to say/ask)
 
 engine=pyttsx3.init("sapi5")#It is used to take voice from users
 voices=engine.getProperty("voices")#An built-in API provides by window to get voices
-print(voices[1].id)
-engine.setProperty("voice",voices[0].id)#Set the voices type for male if we want to change for female then we write [1].id IN print()
+#print(voices[1].id) #It only acknowledge that what type of voices programmer want
+engine.setProperty("voice",voices[1].id)#Set the voices type for male if we want to change for male then we write [0].id IN print()
 
 
 
@@ -18,13 +21,39 @@ def wishMe():#It is used to wish in specific way by using time therefore we have
     if hour>=0 and hour<12:
         speak("Good Morning!")
 
-    elif hour>=12 and hour<16 :
+    elif hour>=12 and hour<15 :
         speak("Good Afternoon!")
+
+    elif hour>=15 and hour<18 :
+        speak("Good Evening!")
 
     else:
         speak("Good Night!")
-    #speak()
+    
+    speak("I am Jarvis Sir,please tell me how I may help you")
+
+def takeCommand():
+    #It takes (Audio) microphone input from the user, convert it to the string and returns string output
+
+    r=sr.Recognizer()
+    with sr.Microphone as source:
+        print("Listening.......")
+        r.pause_threshold = 1 #Wait for seconds of non-speaking audio before a phrace is considered complete(means it deals with recognition time speed)
+        audio=r.listen(source)
+
+    try: #If error occur in recognizing
+        print("Recognizing....")
+        query=r.recognize_google(audio,Language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        #print(e)#It will give the error to recognize the audio of user 
+
+        print("Say that again please....")
+        return "None"#Return NONE as a string only if any problem occur
+    return query
 
 if __name__ == "__main__": # main fn()
-    speak("Ajeet is a good boy")#fn() calling
+    speak("Hello Sir")#fn() calling
     wishMe()
+    takeCommand()
